6c0de5c8397538e32be90bcf549b6f0f1edb71b3
Herman Ras <hermanr@PetraGroup.local>
2019-05-15 12:50:17 +0200

    Fixed Margin Spacing

--- menu.php
+++ menu.php
@@ -12,12 +12,13 @@ include_once './inc/AuthSession.php';
         <?php include_once './inc_header.php'; ?>
         <form id="contact" action="index.php" method="post">
             <h3>Menu</h3>
-            <h4>Health Made Easy</h4>
+            <h4>Health Tracking Made Easy</h4>
             <?php
         if ($_SESSION["AuthLevel"] <= 2){ ?>
             <fieldset>
                 <button type="button" onclick="location.href = 'new_lookup.php';">Add New Employee</button>
             </fieldset>
+            <hr><br>
             <?php }
         if ($_SESSION["AuthLevel"] <= 4){  
        ?>
@@ -25,6 +26,11 @@ include_once './inc/AuthSession.php';
                 <button type="button" onclick="location.href = 'Update_lookup.php';">Update Employee Health
                     Status</button>
             </fieldset>
+            <fieldset>
+                <button type="button" onclick="location.href = 'Walkin_lookup.php';">Add Walking Health
+                    Status</button>
+            </fieldset>
+            <hr><br>
             <?php }
         if ($_SESSION["AuthLevel"] == 0){  
        ?>
@@ -34,8 +40,8 @@ include_once './inc/AuthSession.php';
             <fieldset>
                 <button type="button" onclick="location.href = 'admin.php';">User Administration</button>
             </fieldset>
+            <hr><br>
             <?php } ?>
-            <br><br>
             <fieldset>
                 <button name="submit" type="button" onclick="location.href = 'index.php';">Logoff</button>
             </fieldset>
